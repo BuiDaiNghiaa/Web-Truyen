4ba905eca5d4cebbbc41ceb1e4761c4fea082774
nguyen646 <nanhnguyen6789@gmail.com>
2024-11-05 20:15:38 +0700

    reponsive-404

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@ const menuParSub1 = document.querySelector(".par-menu1");
 const menuSub1 = document.querySelector(".sub-menu1");
 const menuSub2 = document.querySelector(".par-slide-menu");
 const menuSub3 = document.querySelector(".slide-menu3");
+const menuSub4 = document.querySelector(".dropdown-menu");
+
 if(menuParSub1) {
     menuParSub1.addEventListener("click", () => {
         menuSub1.classList.toggle("active");
         menuSub2.classList.remove("active");
         menuSub3.classList.remove("active");
+        menuSub4.classList.remove("active");
     })
 }
 
@@ -16,6 +19,7 @@ if(menuParSub2) {
         menuSub2.classList.toggle("active");
         menuSub1.classList.remove("active");
         menuSub3.classList.remove("active");
+        menuSub4.classList.remove("active");
     })
 }
 const menuParSub3 = document.querySelector(".par-menu3");
@@ -24,5 +28,30 @@ if(menuParSub3) {
         menuSub3.classList.toggle("active");
         menuSub2.classList.remove("active");
         menuSub1.classList.remove("active");
+        menuSub4.classList.remove("active");
     })
 }
+
+const menuParSub4 = document.querySelector(".par-menu4");
+if(menuParSub4){
+    menuParSub4.addEventListener("click", () => {
+        menuSub4.classList.toggle("active");
+        menuSub1.classList.remove("active");
+        menuSub2.classList.remove("active");
+        menuSub3.classList.remove("active");
+    })
+}
+
+// custom
+const select = menuParSub4.querySelector("select");
+if(select){
+    select.addEventListener("click", () => {
+        menuSub4.classList.toggle("active");
+    });
+
+    select.addEventListener("change", () => {
+        menuSub4.classList.remove("active");
+    });
+}
+// End custom
+
