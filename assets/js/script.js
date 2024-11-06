329ef61f47bba784f0f9eb33f140d4da00ae6bbd
DaiNghia <6451071050@st.utc2.edu.vn>
2024-11-06 17:46:41 +0700

    Reponsive header

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,46 @@ menus.forEach(({ button, menu }) => {
     if (button && menu) {
         button.addEventListener("click", (event) => {
             event.stopPropagation();
-            closeAllMenus();  // Đóng tất cả menu trước khi mở menu được nhấn
-            menu.classList.toggle("active");
+            // Kiểm tra nếu menu đã mở
+            const isActive = menu.classList.contains("active");
+
+            if (isActive) {
+                // Nếu menu đã mở, đóng nó
+                menu.classList.remove("active");
+            } else {
+                // Nếu không, đóng tất cả menu trước và mở menu hiện tại
+                closeAllMenus();
+                menu.classList.add("active");
+            }
         });
     }
 });
 
 // Đóng tất cả các menu khi click ra ngoài
 document.addEventListener("click", closeAllMenus);
+
+
+ // Click icon-menu-dropdown 
+ const iconMenuDropdown = document.querySelector(".menu-reponsive");
+ if(iconMenuDropdown) {
+     const ulDropdown = document.querySelector(".menu-drop");
+     iconMenuDropdown.addEventListener("click", () => {
+         ulDropdown.classList.toggle("block");
+     })
+ }
+
+
+ // Select all menu items with a corresponding sub-menu
+ const menuItems = document.querySelectorAll('.menu-edit');
+
+ // Add a click event listener to each main menu item
+ menuItems.forEach((menuItem, index) => {
+     menuItem.addEventListener('click', () => {
+         // Toggle the display of the corresponding sub-menu
+         const subMenu = menuItem.nextElementSibling;
+         if (subMenu && subMenu.classList.contains('sub-menu')) {
+             subMenu.classList.toggle('active');
+             menuItem.classList.toggle('active');
+         }
+     });
+ });
