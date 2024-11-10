687e32e0b4c28785d223ec36111fe0869d2ccc80
DaiNghia <6451071050@st.utc2.edu.vn>
2024-11-10 10:39:48 +0700

    fix-header

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,10 @@ document.addEventListener("click", closeAllMenus);
  const iconMenuDropdown = document.querySelector(".menu-reponsive");
  if(iconMenuDropdown) {
      const ulDropdown = document.querySelector(".menu-drop");
+     const divDropdown = document.querySelector(".header-menu-drop");
      iconMenuDropdown.addEventListener("click", () => {
          ulDropdown.classList.toggle("block");
+         divDropdown.classList.toggle("none");
      })
  }
 
